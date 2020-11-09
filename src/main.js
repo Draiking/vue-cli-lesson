e20150161b40a24b644e5820eba6105c48538719
Gennadya <heavyyrain3039@gmail.com>
2020-11-09 15:50:30 +0300

    1 подключение

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Car from './components/car.vue'
 
-Vue.config.productionTip = false
+Vue.component('app-car', Car)
 
 new Vue({
   render: h => h(App),
