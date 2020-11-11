5563efd6a330592f8fbf952301c1371f9e98cdd5
Gennadya <heavyyrain3039@gmail.com>
2020-11-11 22:06:30 +0300

    Передача параметров

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -17,7 +17,7 @@
       <router-link
       class="btn btn-info mt-2"
       tag="button"
-      :to="{name: 'carFull', params: {id: id}}"
+      :to="{name: 'carFull', params: {id: id}, query: {name: 'Mazda', year: 2000}}"
       >Info</router-link>
 
     <hr />
