9775bd35dd077a2e8724ca731cd0526267f8c57d
Gennadya <heavyyrain3039@gmail.com>
2020-11-11 21:58:55 +0300

    Вложенные роуты

--- src/components/Car.vue
+++ src/components/Car.vue
@@ -1,31 +1,47 @@
 <template>
-    <div>
-        <h1>Car id {{id}}</h1>
+  <div>
+    <h1>Car id {{ id }}</h1>
 
-        <button 
-        class="btn btn-sm btn-info"
-        @click="backToCar"
-        >Back</button>
-    </div>
+    <button class="btn btn-default btn-info mb-2" @click="backToCar">
+      Back
+    </button>
+
+    <br />
+
+    <!-- <router-link
+      class="btn btn-info mt-2"
+      tag="button"
+      :to="'/car/' + id + '/full'"
+      >Info</router-link> -->
+
+      <router-link
+      class="btn btn-info mt-2"
+      tag="button"
+      :to="{name: 'carFull', params: {id: id}}"
+      >Info</router-link>
+
+    <hr />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                // id: this.$router.currentRoute.params['id'] длинная запись
-                id: this.$route.params['id']
-            }
-        },
-        methods: {
-            backToCar() {
-                this.$router.push('/cars')
-            }
-        },
-        watch: {
-            $route(toR) {
-                this.id = toR.params['id']
-            }
-        }
-    }
+export default {
+  data() {
+    return {
+      // id: this.$router.currentRoute.params['id'] длинная запись
+      id: this.$route.params["id"],
+    };
+  },
+  methods: {
+    backToCar() {
+      this.$router.push("/cars");
+    },
+  },
+  watch: {
+    $route(toR) {
+      this.id = toR.params["id"];
+    },
+  },
+};
 </script>
---
+++ src/components/CarFull.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h4>Car name: Ford</h4>
+        <h5>Car Year: 2015</h5>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
