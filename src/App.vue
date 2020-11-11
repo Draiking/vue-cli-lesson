4339eb261e1b8f758a56d57a7a68cd171cbbd48f
Gennadya <heavyyrain3039@gmail.com>
2020-11-11 04:52:12 +0300

    Валидация пароля

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,94 @@
 <template>
-  <div id="app">
+<div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
 
     <div class="container">
-      <form>
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input 
-          type="email"
-          id="email" 
-          class="form-control"
-          :class="{'is-invalid': $v.email.$error}"
-          @blur="$v.email.$touch()"
-          v-model="email" />
-          <div v-if="!$v.email.required"  class="invalid-email">
-            Email field required
-          </div>
-            <div v-if="!$v.email.email"  class="invalid-email">
-            not email
-          </div>
-        </div>
+        <form>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" class="form-control" :class="{'is-invalid': $v.email.$error}" @blur="$v.email.$touch()" v-model="email" />
+                <div v-if="!$v.email.email" class="invalid-email">
+                    not email
+                </div>
+            </div>
 
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" class="form-control" :class="{'is-invalid': $v.password.$error}" @blur="$v.password.$touch()" v-model="password" />
+                <div v-if="!$v.password.minLength" class="invalid-email">
+                    Min Length of password is {{ $v.password.$params.minLength.min }}. Now it is {{ password.length }}
+                </div>
+              
+            </div>
 
-       
-      </form>
+            <div class="form-group">
+                <label for="confirmPassword">Confirm Password</label>
+                <input 
+                type="password" 
+                id="confirmPassword" 
+                class="form-control" 
+                :class="{'is-invalid': $v.confirmPassword.$error}" 
+                @blur="$v.confirmPassword.$touch()" v-model="confirmPassword" />
+                <div v-if="!$v.confirmPassword.sameAs" class="invalid-email">
+                    Пароли не совпадают
+                </div>
+              
+            </div>
+
+        </form>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
-import { required, email } from 'vuelidate/lib/validators'
-
-
+import {
+    required,
+    email,
+    minLength,
+    sameAs
+} from 'vuelidate/lib/validators'
 
 export default {
-  name: "App",
+    name: "App",
 
-  components: {},
-  data() {
-    return {
-      email: ''
-    };
-  },
-  computed: {
+    components: {},
+    data() {
+        return {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        };
+    },
+    computed: {
 
-  },
-  validations: {
-    email: {
-        required,
-        email
+    },
+    validations: {
+        email: {
+            required,
+            email
+        },
+        password: {
+            minLength: minLength(6)
+        },
+        confirmPassword: {
+            sameAs: sameAs('password')
+        }
+    },
+    watch: {
+      password(value) {
+        return value.length
+      }
     }
-  }
 };
 </script>
 
 <style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
